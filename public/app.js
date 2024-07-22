784031578f0cd091b751cdb56d41115ddbd74d56
Valentyne <dr_th@ukr.net>
2024-07-22 17:35:03 +0300

    Add add_to_cart function stub

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 function something()
 {
+
 	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
 	x = x * 1 + 1; // x = x + 1
@@ -7,4 +8,9 @@ function something()
 	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 	alert(x);
+}
+
+function add_to_cart()
+{
+	alert('Hello from function!');
 }
